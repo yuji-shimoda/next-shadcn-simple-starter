3208803b292720d2b3364e5137954be1a913672d
Yuji Shimoda <yuji.shimoda@gmail.com>
2024-12-07 01:05:38 +0900

    feature: add sample signin page.

---
+++ src/app/(auth)/(signin)/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+import SignInViewPage from "../_components/sigin-view";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Authentication | Sign In",
+  description: "Sign In page for authentication.",
+};
+
+export default function Page() {
+  return (
+    <Suspense>
+      <SignInViewPage />
+    </Suspense>
+  );
+}
